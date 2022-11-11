d8bb4fb79b7493ded5dee8bd5dd6702cbbc3f98d
zanmato <zanmato1984@gmail.com>
2022-11-11 21:49:20 +0800

    Init

---
+++ foo.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+void foo() {
+    std::cout << "Set breakpoint on me." << std::endl;
+    std::cout << "Did Clion trigger it?" << std::endl;
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+void foo();
+
+int main() {
+    foo();
+    return 0;
+}
